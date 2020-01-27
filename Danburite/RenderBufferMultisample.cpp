c9f0a41802f82a03c922a0f230823b4b570b89cc
Sein <godsein519@gmail.com>
2020-01-27 11:53:33 +0900

    MSAA 일단은 뜨게 만들어 놓음. 이후는 glsl 문제인듯..

--- Danburite/RenderBufferMultisample.cpp
+++ Danburite/RenderBufferMultisample.cpp
@@ -6,6 +6,7 @@ namespace ObjectGL
 		const GLsizei width, const GLsizei height,
 		const RenderBufferInternalFormatType internalFormat, const GLsizei numSamplePoints) noexcept
 	{
+		bind();
 		glRenderbufferStorageMultisample(
 			GL_RENDERBUFFER, numSamplePoints, GLenum(internalFormat), width, height);
 	}
