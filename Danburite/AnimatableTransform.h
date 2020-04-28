4a80f1a4f3afc0e33240b3b90c3e5346e82717e0
Sein <foryx1992@naver.com>
2020-04-28 23:28:40 +0900

    AnimatableTransform 헤더 생성

--- Danburite/AnimatableTransform.h
+++ Danburite/AnimatableTransform.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "Transform.h"
+
+namespace Danburite
+{
+	class AnimatableTransform
+	{
+	private:
+		Transform __animationTransform;
+		Transform __modelTransform;
+	};
+}
