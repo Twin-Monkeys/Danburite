d44f020c64a7a0683e1074645b19f56c8d905e5a
이세인 <foryx1992@naver.com>
2020-07-06 16:13:14 +0900

    Bloom 작업 진행 중

--- Danburite/BloomPostProcessor.cpp
+++ Danburite/BloomPostProcessor.cpp
@@ -1,7 +1,38 @@
 #include "BloomPostProcessor.h"
 #include "ProgramFactory.h"
+#include "ShaderIdentifier.h"
+#include "TextureUtil.h"
+
+using namespace std;
+using namespace ObjectGL;
 
 namespace Danburite
 {
+	BloomPostProcessor::BloomPostProcessor() :
+		__program(ProgramFactory::getInstance().getProgram(ProgramType::POST_PROCESS_BLOOM))
+	{}
 
+	void BloomPostProcessor::_onRender(UniformBuffer &attachmentSetter, VertexArray &fullscreenQuadVA) noexcept
+	{
+
+
+		__program.bind();
+		fullscreenQuadVA.draw();
+	}
+
+	void BloomPostProcessor::setScreenSize(const GLsizei width, const GLsizei height) noexcept
+	{
+		__pColorAttachment->memoryAlloc(
+			width, height, TextureInternalFormatType::RGB16F, TextureExternalFormatType::RGB);
+
+		__pExtractedColorAttachment->memoryAlloc(
+			width, height, TextureInternalFormatType::RGB16F, TextureExternalFormatType::RGB);
+
+		__pDepthStencilAttachment->memoryAlloc(
+			width, height, RenderBufferInternalFormatType::DEPTH24_STENCIL8);
+
+		_attach(AttachmentType::COLOR_ATTACHMENT0, *__pColorAttachment);
+		_attach(AttachmentType::COLOR_ATTACHMENT1, *__pExtractedColorAttachment);
+		_attach(AttachmentType::DEPTH_STENCIL_ATTACHMENT, *__pDepthStencilAttachment);
+	}
 }
--- Danburite/BloomPostProcessor.h
+++ Danburite/BloomPostProcessor.h
@@ -1,11 +1,26 @@
 #pragma once
 
+#include "PostProcessor.h"
+#include "AttachableTexture2D.h"
+#include "RenderBuffer.h"
+
 namespace Danburite
 {
-	class BloomPostProcessor
+	class BloomPostProcessor : public PostProcessor
 	{
+	private:
+		ObjectGL::Program &__program;
+		std::unique_ptr<ObjectGL::AttachableTexture2D> __pColorAttachment;
+		std::unique_ptr<ObjectGL::AttachableTexture2D> __pExtractedColorAttachment;
+		std::unique_ptr<ObjectGL::RenderBuffer> __pDepthStencilAttachment;
+
+	protected:
+		virtual void _onRender(
+			ObjectGL::UniformBuffer &attachmentSetter, ObjectGL::VertexArray &fullscreenQuadVA) noexcept override;
+
 	public:
 		BloomPostProcessor();
+		virtual void setScreenSize(const GLsizei width, const GLsizei height) noexcept override;
 
 		virtual ~BloomPostProcessor() = default;
 	};
