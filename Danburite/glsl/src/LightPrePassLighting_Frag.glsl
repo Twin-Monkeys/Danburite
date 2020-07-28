3feb16b2f482e597d29627e7f47d56327162ec6a
이세인 <foryx1992@naver.com>
2020-07-28 11:45:41 +0900

    LightPrePass Lighting Pass까지 완성

--- Danburite/glsl/src/LightPrePassLighting_Frag.glsl
+++ Danburite/glsl/src/LightPrePassLighting_Frag.glsl
@@ -12,11 +12,25 @@ layout (location = 2) out vec3 specular;
 void main()
 {
 	const uint curLightIdx = LightPrePass_getCurrentLightIdx();
-
 	const sampler2DRect posTex = TextureContainer_getTextureAs2DRect(0);
 	const sampler2DRect normal3_shininess1Tex = TextureContainer_getTextureAs2DRect(1);
 
-	ambient3_attenuation1 = texture(normal3_shininess1Tex, gl_FragCoord.xy);
-	diffuse3_occlusionInv1 = vec4(0.f);
-	specular = vec3(0.f);
+	const vec4 normal3_shininess1 = texture(normal3_shininess1Tex, gl_FragCoord.xy);
+
+	const vec3 targetPos = texture(posTex, gl_FragCoord.xy).xyz;
+	const vec3 targetNormal = normal3_shininess1.xyz;
+	const float shininess = normal3_shininess1.w;
+
+	const vec3 ambient = Light_getLightAmbient(curLightIdx, targetPos);
+	const float attenuation = Light_getAttenuation(curLightIdx, targetPos);
+	ambient3_attenuation1 = vec4(ambient, attenuation);
+
+	const vec3 diffuse = Light_getLightDiffuse(curLightIdx, targetPos, targetNormal);
+	const float occlusion = Light_getOcclusion(curLightIdx, targetPos, targetNormal);
+	const float occlusionInv = (1.f - occlusion);
+	diffuse3_occlusionInv1 = vec4(diffuse, occlusionInv);
+
+	const vec3 viewPos = Camera_getPosition();
+	const vec3 viewDir = normalize(viewPos - targetPos);
+	specular = Light_getLightSpecular(curLightIdx, targetPos, targetNormal, viewDir, shininess);
 } 
